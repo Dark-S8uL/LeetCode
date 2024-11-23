935c967eacbc4ea2918a82df81f9a9aad86fa743
Priyanshu Rathi <167284441+Dark-S8uL@users.noreply.github.com>
2024-11-23 11:11:48 +0530

    POTD 20-11-24

--- 2516. Take K of Each Character From Left and Right.cpp
+++ 2516. Take K of Each Character From Left and Right.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int takeCharacters(string s, int k) {
         unordered_map<char, int> mp, w;
-        for (char c : s) mp[c]++;
-        if (mp['a'] < k || mp['b'] < k || mp['c'] < k) return -1;
+        for(char c : s) mp[c]++;
+        if(mp['a'] < k || mp['b'] < k || mp['c'] < k) return -1;
         int n = s.size();
         int mini = n, l = 0, r = 0;
         while(r < n){
             w[s[r]]++;
-            while (mp['a'] - w['a'] < k || mp['b'] - w['b'] < k || mp['c'] - w['c'] < k) {
+            while(mp['a'] - w['a'] < k || mp['b'] - w['b'] < k || mp['c'] - w['c'] < k) {
                 w[s[l]]--;
                 l++;
             }
